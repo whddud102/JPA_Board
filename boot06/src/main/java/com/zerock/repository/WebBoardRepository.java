f9f2c23451eafe09a6a9256a6b0701674744a190
JongYoung <whddud102@naver.com>
2020-05-27 18:28:42 +0900

    WebBoardRepository.java 수정
    - Predicate를 파라미터로 전달하기 위해서 QuerydslPredicateExecutor 인터페이스를 상속

--- boot06/src/main/java/com/zerock/repository/WebBoardRepository.java
+++ boot06/src/main/java/com/zerock/repository/WebBoardRepository.java
@@ -1,9 +1,10 @@
 package com.zerock.repository;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import com.zerock.domain.WebBoard;
 
-public interface WebBoardRepository  extends CrudRepository<WebBoard, Long>{
+public interface WebBoardRepository  extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard>{
 
 }
