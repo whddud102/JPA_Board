a141ecaf20b4f8d622a2ac4601d26ea04b0d2f15
JongYoung <whddud102@naver.com>
2020-05-28 11:38:53 +0900

    검색 조건 처리 기능 추가
    WebBoardRepository.java
    - 검색 조건 별로 동적으로 쿼리를 생성하도록 수정
    
    WebBoardRepositoryTests.java
    - 테스트 코드 작성

--- boot06/src/main/java/com/zerock/repository/WebBoardRepository.java
+++ boot06/src/main/java/com/zerock/repository/WebBoardRepository.java
@@ -8,13 +8,12 @@ import com.querydsl.core.types.Predicate;
 import com.zerock.domain.QWebBoard;
 import com.zerock.domain.WebBoard;
 
-public interface WebBoardRepository  extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard>{
-
-	
+public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard> {
 
 	/**
 	 * 검색 키워드와, 검색 타입을 전달 받아서 Predicate 객체를 생성해주는 메서드
-	 * @param type 검색 타입
+	 * 
+	 * @param type    검색 타입
 	 * @param keyword 검색 키워드
 	 * @return Predicate 객체
 	 */
@@ -22,13 +21,30 @@ public interface WebBoardRepository  extends CrudRepository<WebBoard, Long>, Que
 	public default Predicate makePredicate(String type, String keyword) {
 		// Predicate 객체는 Boolean Builder를 통해 생성
 		BooleanBuilder builder = new BooleanBuilder();
-		
+
 		// Qdomain 클래스를 이용해 Qdomain 객체에 접근
 		QWebBoard board = QWebBoard.webBoard;
 
 		// bno > 0 이상에 해당하는 데이터를 가져오는 조건 추가 - And 조건
-		builder.and(board.bno.gt(0));		// board 객체의 bno 가 0보다 크냐? 조건 추가
-		
+		builder.and(board.bno.gt(0)); // board 객체에 'bno > 0' 조건 추가
+
+		if (type == null) {
+			// 검색 조건이 null 이면 bno > 0 조건만 추가한 채 predicate 반환
+			return builder;
+		}
+
+		switch (type) {
+		case "t":	// 제목 검색인 경우
+			builder.and(board.title.like("%" + keyword + "%"));
+			break;
+		case "c":	// 내용 검색인 경우
+			builder.and(board.content.like("%" + keyword + "%"));
+			break;
+		case "w":	// 작성자 검색인 경우
+			builder.and(board.writer.like("%" + keyword + "%"));
+			break;
+		}
+
 		return builder;
 	}
 }
--- boot06/src/test/java/com/zerock/WebBoardRepositoryTests.java
+++ boot06/src/test/java/com/zerock/WebBoardRepositoryTests.java
@@ -19,39 +19,53 @@ import com.zerock.repository.WebBoardRepository;
 import lombok.extern.java.Log;
 
 @Log
-@Commit	// 테스트 결과를 DB에 반영하도록 설정
+@Commit // 테스트 결과를 DB에 반영하도록 설정
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebBoardRepositoryTests {
-	
+
 	@Autowired
 	private WebBoardRepository repo;
-	
+
 	// 더미 게시글 데이터 추가
 	@Test
 	public void insertBoardDummies() {
 		IntStream.range(0, 300).forEach(i -> {
 			WebBoard board = new WebBoard();
-			
+
 			board.setTitle("sample Board Title " + i);
 			board.setContent("Content Sample ... " + i + "of Board");
-			board.setWriter("user0" + (i%10));
-			
+			board.setWriter("user0" + (i % 10));
+
 			repo.save(board);
 		});
 	}
-	
+
 	@Test
 	public void testList1() {
 		// bno 속성을 기준으로 내림차순 정렬하는 Pageable 구현체 생성
 		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
-		
+
 		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), pageable);
-		
+
 		log.info("PAGE : " + result.getPageable());
-		
+
 		log.info("--------------------------------------");
 		result.getContent().forEach(board -> log.info("" + board));
 	}
-	
+
+	@Test
+	public void testList2() {
+		// bno 속성을 기준으로 내림차순 정렬하는 Pageable 구현체 생성
+		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+
+		// 제목에 10이 들어가는 게시글 검색
+		Page<WebBoard> result = repo.findAll(repo.makePredicate("t", "10"), pageable);
+
+		log.info("PAGE : " + result.getPageable());
+
+		log.info("--------------------------------------");
+		result.getContent().forEach(board -> log.info("" + board));
+	}
+
 }
